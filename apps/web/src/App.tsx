46e25e57d4092b3e6f061848585204ab8228486d
Eldo Macuácua <eldomacuacua@yahoo.com>
2025-06-30 13:49:17 +0530

    add: updates 2

--- apps/web/src/App.tsx
+++ apps/web/src/App.tsx
@@ -5,8 +5,8 @@ import Features from './components/layout/features'
 import Solutions from './components/layout/solutions'
 import Results from './components/layout/results'
 import UseCases from './components/layout/useCases'
-import Pricing from './components/layout/Pricing'
-import CTA from './components/layout/CTA'
+import Pricing from './components/layout/pricing'
+import CTA from './components/layout/cta'
 import Footer from './components/layout/Footer'
 
 function App() {
