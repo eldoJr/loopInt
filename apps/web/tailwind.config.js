78f5d023d21f1fd3e358bc031450b406596f2c71
Eldo Macuácua <eldomacuacua@yahoo.com>
2025-08-11 08:35:37 +0530

    till

--- apps/web/tailwind.config.js
+++ apps/web/tailwind.config.js
@@ -5,8 +5,34 @@ module.exports = {
   theme: {
     extend: {
       fontFamily: {
-        sans: ['"Open Sans"', 'sans-serif'],
-        serif: ['"Cormorant Garamond"', 'serif'],
+        sans: ['Comfortaa', 'sans-serif'],
+        comfort: ['Comfortaa', 'sans-serif'],
+      },
+      colors: {
+        'tech-orange': {
+          50: '#fff7ed',
+          100: '#ffedd5',
+          200: '#fed7aa',
+          300: '#fdba74',
+          400: '#fb923c',
+          500: '#f97316',
+          600: '#ea580c',
+          700: '#c2410c',
+          800: '#9a3412',
+          900: '#7c2d12',
+        },
+        'tech-purple': {
+          50: '#faf5ff',
+          100: '#f3e8ff',
+          200: '#e9d5ff',
+          300: '#d8b4fe',
+          400: '#c084fc',
+          500: '#a855f7',
+          600: '#9333ea',
+          700: '#7c3aed',
+          800: '#6b21a8',
+          900: '#581c87',
+        },
       },
     },
   },
